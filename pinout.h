717b6e217b6d9c7a01193099b2c18046fe6687ab
Steve McGrath <smcgrath23@gmail.com>
2012-05-11 10:38:28 -0500

    more pin assignments

--- pinout.h
+++ pinout.h
@@ -3,9 +3,12 @@
 #ifndef _PINOUT_H_
 #define _PINOUT_H_
 
-#define P_LED 13
-
+#define P_ESTOP_IN 4
+#define P_ESTOP_OUT 5
+#define P_SELECT_IN 6
+#define P_SELECT_OUT 7
 #define P_JOY_X 9  // Joystick X-axis signal
 #define P_JOY_Y 10  // Joystick Y-axis signal
 
+#define P_LED 13
 #endif
