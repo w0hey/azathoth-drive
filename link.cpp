42c36bd418dc4a89af623e948c77e3cd5fc41398
Steve McGrath <smcgrath23@gmail.com>
2012-05-20 23:09:37 -0500

    copy in revised link code from azathoth-mega

--- link.cpp
+++ link.cpp
@@ -2,13 +2,34 @@
 
 #include "link.h"
 #include <Arduino.h>
+#include "errors.h"
 
-Link::Link(void (*function)(int, byte*)) {
+Link::Link(void (*errhandler)(byte)) {
+  nHandlers = 0;
   pos = 0;
   len = 0;
   xor_next = false;
-  callback = function;
+  errHandler = errhandler;
   Serial.begin(115200);
+}
+
+// set a handler for a given command byte.
+// takes a pointer to a function which will be called when the command is received
+void Link::setHandler(byte cmd, void(*handler)(byte, byte*)) {
+  nHandlers++;
+  // grow the array to accept the new handler
+  handler_t* ptr = (handler_t*)realloc(handlers, sizeof(handler_t) * nHandlers);
+  if (ptr == NULL) {
+    // realloc failed
+    nHandlers--;
+    errHandler(E_MALLOC);
+    return;
+  }
+  handlers = ptr;
+  handlers[nHandlers - 1].command = cmd;
+  handlers[nHandlers - 1].handler = handler;
+  // bsearch, used in getHandler, requires that the array is sorted
+  qsort(handlers, nHandlers, sizeof(handler_t), compare);
 }
 
 
@@ -18,7 +39,6 @@ void Link::service() {
   while (Serial.available()) {
     // take a byte
     recv = Serial.read();
-    
     if (pos == 0) {
       // This should be a start byte, otherwise something is wrong
       if (recv != 0x7e) {
@@ -26,43 +46,36 @@ void Link::service() {
         continue;
       }
     }
-    
     if (pos == 1) {
       // This is the length field, store it.
       len = recv;
     }
-    
     if ((pos != 0) && (recv == 0x7e)) {
       // This is an unexpected start byte, so reset and move on
       len = 0;
       pos = 0;
       xor_next = 0;
     }
-    
     // check to see if we need to unescape the next byte
     if (recv == 0x7d) {
       // discard this byte and unescape the next one
       xor_next = true;
       continue;
     }
-    
     // check to see if we need to unescape this byte
     if (xor_next) {
       recv = recv ^ 0x20;
       xor_next = false;
     }
-    
     // store the byte
     packet[pos] = recv;
-    
     // check if we're done with this packet
     if (pos == len + 1) {
-      callback(len, packet);
+      dispatch(len, packet);
       pos = 0;
       len = 0;
       continue;
     }
-    
     // on to the next byte
     pos++;
   }
@@ -93,4 +106,46 @@ void Link::buildPacket(byte size, byte data[]) {
   for (int i = 0; i <= size; i++) {
     packet_out[2 + i] = data[i];
   }
+}
+
+// looks up the appropriate handle for a command and calls it
+void Link::dispatch(byte length, byte* packet) {
+  byte cmd = packet[2];
+  byte len = packet[1] - 1;
+  byte *data = (byte*)malloc(len * sizeof(byte));
+  if (data == NULL){
+    // malloc failed
+    errHandler(E_MALLOC);
+  }
+  memcpy(data, packet + 3, len);
+  void (*h)(byte, byte*) = getHandler(cmd);
+  if (h == NULL) {
+    errHandler(E_WTF);
+  }
+  h(len, data);
+  free(data);
+}
+
+// finds the correct handler for a command in our array of handlers
+void (*Link::getHandler(byte cmd))(byte, byte*) {
+  handler_t h = *(handler_t*)bsearch(&cmd, handlers, nHandlers, sizeof(handler_t), compare_key);
+    if (&h == NULL) {
+      //nothing!
+      return NULL;
+    }
+    return h.handler;
+}
+
+// comparison function for finding a specific handler with bsearch
+int Link::compare_key(const void *p1, const void *p2) {
+  byte k = *(byte*)p1;
+  handler_t h = *(handler_t*)p2;
+  return (k - h.command);
+}
+
+// comparison function for sorting our array with qsort
+int Link::compare(const void *p1, const void *p2) {
+  handler_t h1 = *(handler_t*)p1;
+  handler_t h2 = *(handler_t*)p2;
+  return (h1.command - h2.command);
 }
--- link.h
+++ link.h
@@ -1,23 +1,38 @@
-// Header file for BB->Arduino datalink
+// Header file for BB->Mega datalink
 
 #ifndef _LINK_H_
 #define _LINK_H_
 #include <Arduino.h>
 
+typedef struct {
+  byte command;
+  void (*handler)(byte, byte*);
+} handler_t;
+
 class Link {
   public:
-    Link(void (*function)(int, byte*));
+    Link(void(*)(byte));
+    void setHandler(byte, void(*)(byte, byte*));
     void service();
     void sendData(int, byte*);
   private:
-    byte packet[255];
-    byte packet_out[255];
+    byte packet[255]; // buffer for incoming packet
+    byte packet_out[255]; // buffer for outgoing packet
     byte recv;
     int pos;
     byte len;
     boolean xor_next;
-    void (*callback)(int, byte*);
+    
+    int nHandlers; // number of registered command handlers
+    handler_t* handlers; // array of command handlers
+    void (*errHandler)(byte); // pointer to a function to call on error
+    
     void buildPacket(byte, byte*);
+    void dispatch(byte, byte*);
+    void (*getHandler(byte))(byte, byte*);
+    
+    static int compare_key(const void*, const void*);
+    static int compare(const void*, const void*);
 };
 
 #endif
